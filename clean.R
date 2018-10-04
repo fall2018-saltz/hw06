b040217ce9de1bf0cad74e5a1cd847e6e983dae9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:47:24 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,10 +3,10 @@ raw_data
 my_func<-function(states)
 {
  states<-states[c(-1,-53),c(-1:-4)]
- colnames(states)[1]<-"stateName"
- colnames(states)[2]<-"population"
- colnames(states)[3]<-"popOver18"
- colnames(states)[4]<-"percentOver18"
+ #colnames(states)[1]<-"stateName"
+ #colnames(states)[2]<-"population"
+ #colnames(states)[3]<-"popOver18"
+ #colnames(states)[4]<-"percentOver18"
  return(states)
 }
 
