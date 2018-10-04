8ef40255116389044bee0ce253b7ed498adc26f1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:42:15 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,5 +1,5 @@
 
-clean_data <- raw_data
+
 my_func<-function(states)
 {
   states<-states[-1,]
@@ -8,5 +8,5 @@ my_func<-function(states)
   return (states)
 }
 
-my_func(raw_data)
-raw_data
+clean_data<-my_func(raw_data)
+clean_data
