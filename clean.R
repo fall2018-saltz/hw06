80f0ab9debfbe3fa60887ff5667138704ccf8ba8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:56:52 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -28,4 +28,4 @@ mergedDf<-merge(clean_data,arrests,by="row.names",all=TRUE)
 mergedDf<- mergedDf[,-c(1)]
 mergedDf
 
-geom(population,aes(x=Population))+geom_histogram()
+ggplot(mergedDf,aes(x=population))+geom_histogran()
